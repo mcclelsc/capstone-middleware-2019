81802b4f3b976094dbb0b676b3ef0ef12383d35b
Scott McClelland <mcclelsc@sheridancollege.ca>
2019-05-26 23:11:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,8 @@ app.post('/getDocumentId', (req, res1) => {
 	var queryParams = {
 	  environment_id: 'a81bea55-c449-4499-8c7b-4cd3358ea94d',
 	  collection_id: '89949583-2061-48d0-ade2-289ed65a499a',
-	  query:insertModuleJSON.filename
+	  natural_language_query:insertModuleJSON.filename,
+	  passages:false
 	};
 
 	discovery.query(queryParams)
