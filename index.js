0ce851bd3457724c935d3084605598e194493391
Scott McClelland <mcclelsc@sheridancollege.ca>
2019-05-26 23:03:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.post('/continueConversation', (req, res1) => {
 
 			chatText = chatObject.output.generic[0].text + "";
 			
-			if (chatObject.output.entities.length > 0 && chatText === "specificDiscoveryQuery"){
+			if (chatObject.output.entities.length > 0 && chatText === "Give me a moment to find that report."){
 				chatText += ";uniqueDelimiter;" + chatObject.output.entities[0].value;
 			}
 
