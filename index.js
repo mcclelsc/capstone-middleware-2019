d75a9dea8ed20f4d065b81424757f0438365f830
Scott McClelland <mcclelsc@sheridancollege.ca>
2019-06-03 13:43:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -74,8 +74,14 @@ app.post('/continueConversation', (req, res1) => {
 		.then(res => {
 			chatText = JSON.stringify(res, null, 2);
 			chatObject = JSON.parse(chatText);
-			console.log(chatObject.output);
-			chatText = chatObject.output.generic[0].text;
+			
+			if (chatObject.output.generic.length > 0){
+				chatText = chatObject.output.generic[0].text;
+			}
+			else{
+				chatText = "I could not process your statement. Please try again."
+			}
+			
 			
 			if (chatObject.output.entities.length > 0 && chatText === "Give me a moment to find that report."){
 				chatText += ";uniqueDelimiter;" + chatObject.output.entities[0].value;
