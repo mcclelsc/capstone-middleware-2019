19c879e56f814b0ffabbedb46b834b380008d677
Scott McClelland <mcclelsc@sheridancollege.ca>
2019-06-05 14:38:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.post('/continueConversation', (req, res1) => {
 				chatText = "I could not process your statement. Please try again."
 			}
 			
-			
+			console.log(chatObject);
 			if (chatObject.output.entities.length > 0 && chatObject.output.intents.length > 0 && chatObject.output.intents[0].intent === "RecognizeReport"){
 				chatText += ";uniqueDelimiter;" + chatObject.output.entities[0].value;
 			}
