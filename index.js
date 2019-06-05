b61a398e8aeb7d722ef01bf1e0580df3a84ac3b6
Scott McClelland <mcclelsc@sheridancollege.ca>
2019-06-05 11:19:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -249,15 +249,7 @@ app.post('/uploadDocument', (req, res1) => {
 	//Unpack payload's body into workable object
 	//var insertModuleJSON = JSON.parse(Object.keys(req.body)[0]);
 	
-	//console.log(req.files);
-	
-	var chunks = [];
-	
-	res1.on('data', chunk => chunks.push(Buffer.from(chunk))).on('end', () =>{
-		var buffer = Buffer.concat(chunks);
-		res1.status(200);
-		console.log(buffer.toString('base64'));
-	});
+	console.log(req.body);
 	
 	/*var documentParams = {
 	  environment_id: discoveryEnvironmentID,
